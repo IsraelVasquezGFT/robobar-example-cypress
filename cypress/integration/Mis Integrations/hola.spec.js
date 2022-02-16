2dff5c39d3db05f014200841fad8215bbcd91378
Israel Vasquez <ilve@gft.com>
2022-02-16 12:13:24 +0100

    first commit

--- cypress/integration/Mis Integrations/hola.spec.js
+++ cypress/integration/Mis Integrations/hola.spec.js
@@ -7,19 +7,37 @@
 
 // functions
 
-function colaButton(){
+/*function colaButton(){
     return cy.get(':nth-child(1) > :nth-child(3) > .row > .col-5 > .input-group-append > .btn');
-}
+}*/
 
 function totalGetText(){
     return cy.get(':nth-child(4) > .ng-binding');
 }
 
-/*function paramsColaButton(numClicks){
+function orderGetText(){
+    return cy.get('.btn-success')
+}
+
+function paramsColaButton(numClicks){
     for(var x=0; x<numClicks;x++) {
         cy.get(':nth-child(1) > :nth-child(3) > .row > .col-5 > .input-group-append > .btn').click();
     }
-}*/
+}
+
+function paramsBeerButton(numClicks){
+    for(var x=0; x<numClicks;x++) {
+        cy.get(':nth-child(2) > :nth-child(3) > .row > .col-5 > .input-group-append > .btn').click();
+    }
+}
+
+function paramsWineButton(numClicks){
+    for(var x=0; x<numClicks;x++) {
+        cy.get(':nth-child(3) > :nth-child(3) > .row > .col-5 > .input-group-append > .btn').click();
+    }
+}
+
+
 
 // Cypress
 
@@ -27,19 +45,92 @@ describe('Hello Cypress', () => {
     it('Says hello', () => {
       expect(true).to.equal(true)
     })
+    // - - - - - - - - - - Cola - - - - - - - - - -
     it('Order a Cola', () => {
         cy.visit('http://localhost:3000/#!/')
-        colaButton().click();
+        paramsColaButton(1);
         totalGetText().should("contain.text","€1.25")
+        cy.get('.col-12 > .btn').click()
+        orderGetText().should("contain.text","Order")
+        orderGetText().click()
+        cy.get('p').should("contain.text","Coming right up! ~bzzzt~")
     })
+
     it('Order Two Colas', () => {
         cy.visit('http://localhost:3000/#!/')
-        colaButton().click().click()
+        paramsColaButton(2);
         totalGetText().should("contain.text","€2.50")
+        cy.get('.col-12 > .btn').click()
+        orderGetText().should("contain.text","Order")
+        orderGetText().click()
+        cy.get('p').should("contain.text","Coming right up! ~bzzzt~")
     })
-    /*it('Order Two Colas Params', () => {
+
+    // - - - - - - - - - - Beer - - - - - - - - - -
+    it('Order One Beer', () => {
         cy.visit('http://localhost:3000/#!/')
-        paramsColaButton(3)
-        totalGetText().should("contain.text","€3.75")
-    })*/
+        paramsBeerButton(1)
+        totalGetText().should("contain.text","€2.00")
+        cy.get('.col-12 > .btn').click()
+        cy.get('.col-2').should("contain.text","Age")
+        cy.get('.btn-success').should('be.disabled')
+        cy.get('#ageInput').type("17")
+        cy.get('.btn-success').should('be.enabled')
+        cy.get('.btn-success').click()
+        cy.get('.alert > .ng-binding').should("contain.text","Only adults can buy alcohol!")
+        cy.get('#ageInput').clear()
+        cy.get('#ageInput').type("19")
+        cy.get('.btn-success').should('be.enabled')
+        cy.get('.btn-success').click()
+    })
+    it('Order Two Beer', () => {
+        cy.visit('http://localhost:3000/#!/')
+        paramsBeerButton(2)
+        totalGetText().should("contain.text","€4.00")
+        cy.get('.col-12 > .btn').click()
+        cy.get('.col-2').should("contain.text","Age")
+        cy.get('.btn-success').should('be.disabled')
+        cy.get('#ageInput').type("17")
+        cy.get('.btn-success').should('be.enabled')
+        cy.get('.btn-success').click()
+        cy.get('.alert > .ng-binding').should("contain.text","Only adults can buy alcohol!")
+        cy.get('#ageInput').clear()
+        cy.get('#ageInput').type("19")
+        cy.get('.btn-success').should('be.enabled')
+        cy.get('.btn-success').click()
+    })
+
+    // - - - - - - - - - - Wine - - - - - - - - - -
+    it('Order One Wine', () => {
+        cy.visit('http://localhost:3000/#!/')
+        paramsWineButton(1)
+        totalGetText().should("contain.text","€3.00")
+        cy.get('.col-12 > .btn').click()
+        cy.get('.col-2').should("contain.text","Age")
+        cy.get('.btn-success').should('be.disabled')
+        cy.get('#ageInput').type("17")
+        cy.get('.btn-success').should('be.enabled')
+        cy.get('.btn-success').click()
+        cy.get('.alert > .ng-binding').should("contain.text","Only adults can buy alcohol!")
+        cy.get('#ageInput').clear()
+        cy.get('#ageInput').type("19")
+        cy.get('.btn-success').should('be.enabled')
+        cy.get('.btn-success').click()
+    })
+    it('Order Two Wine', () => {
+        cy.visit('http://localhost:3000/#!/')
+        paramsWineButton(2)
+        totalGetText().should("contain.text","€6.00")
+        cy.get('.col-12 > .btn').click()
+        cy.get('.col-2').should("contain.text","Age")
+        cy.get('.btn-success').should('be.disabled')
+        cy.get('#ageInput').type("17")
+        cy.get('.btn-success').should('be.enabled')
+        cy.get('.btn-success').click()
+        cy.get('.alert > .ng-binding').should("contain.text","Only adults can buy alcohol!")
+        cy.get('#ageInput').clear()
+        cy.get('#ageInput').type("19")
+        cy.get('.btn-success').should('be.enabled')
+        cy.get('.btn-success').click()
+    })
 })
